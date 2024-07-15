598e056d46199611ebb8bdcea82289c26b9d37a6
pablo <pabloccsanchez@gmail.com>
2024-07-15 17:14:20 -0500

    feat: Agregar dependencias indirectas y añadir funciones utilitarias para estilos y tablas

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/pablos-juan/managerdetareas
 go 1.22.0
 
 require (
+	github.com/alexeyco/simpletable v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
---
+++ utilities/table.go
@@ -0,0 +1,60 @@
+package utilities
+
+import (
+	"fmt"
+
+	"github.com/alexeyco/simpletable"
+)
+
+func PrintTable(tasks []Task) {
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "ID"},
+			{Align: simpletable.AlignCenter, Text: "Tarea"},
+			{Align: simpletable.AlignCenter, Text: "Completada"},
+		},
+	}
+
+	var tn string
+	var ts string
+	for i, v := range tasks {
+		tn = Blue(v.Name)
+		ts = fmt.Sprintf("%t", v.Done)
+		ts = Blue(ts)
+		if v.Done {
+			tn = Green(ts)
+			ts = Green(ts)
+		}
+
+		r := []*simpletable.Cell{
+			{Align: simpletable.AlignRight, Text: fmt.Sprint(i + 1)},
+			{Text: tn},
+			{Align: simpletable.AlignCenter, Text: ts},
+		}
+
+		table.Body.Cells = append(table.Body.Cells, r)
+	}
+
+	tx := fmt.Sprintf("Tiene %d tareas por terminar", IncompleteTasks(tasks))
+	table.Footer = &simpletable.Footer{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Span: 3, Text: Red(tx)},
+		},
+	}
+
+	table.SetStyle(simpletable.StyleUnicode)
+	fmt.Println(table.String())
+}
+
+func IncompleteTasks(tasks []Task) int {
+	n := 0
+	for _, v := range tasks {
+		if !v.Done {
+			n++
+		}
+	}
+	return n
+}
