6fefb681c767b6b53e8a7852d8f102001bff137b
pablo <pabloccsanchez@gmail.com>
2024-07-16 10:37:24 -0500

    chore: Eliminar tarea de la lista de tareas pendientes

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,13 +1,32 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 github.com/pablos-juan/managerdetareas
 */
 package cmd
 
 import (
-	"fmt"
+	"strconv"
 
+	"github.com/pablos-juan/managerdetareas/utilities"
 	"github.com/spf13/cobra"
 )
+
+func deleteTask(i int) {
+	tasks, err := utilities.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	if i < 0 || i > len(tasks) {
+		utilities.ErrorMessage("Error. '%d' no está dentro de las tareas!", i)
+		return
+	}
+
+	tasks = append(tasks[:i-1], tasks[i:]...)
+	err = utilities.Save(tasks)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
@@ -15,7 +34,17 @@ var deleteCmd = &cobra.Command{
 	Short: "Borra una tarea de la lista",
 	Long:  `Borra una tarea de la lista de tareas pendientes.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
+		if len(args) == 1 {
+			i, err := strconv.Atoi(args[0])
+			if err != nil {
+				utilities.ErrorMessage("Error. '%s' no es un número!", args[0])
+				return
+			}
+
+			deleteTask(i)
+			return
+		}
+		utilities.ErrorMessage("Error. Necesita un número entero como argumento!")
 	},
 }
 
